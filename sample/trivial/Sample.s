9ec79179a412e63250bf520b6d8d32cd008cedac
Dmytro <dm.vict.kr@gmail.com>
2021-01-27 07:13:37 +0200

    Fix sample, use module in sample

--- sample/trivial/Sample.s
+++ sample/trivial/Sample.s
@@ -1,18 +1,3 @@
-
-// let _ = require( 'wresolverextra' );
-// var src =
-// {
-//   dir :
-//   {
-//     val1 : 'Hello'
-//   },
-// }
-
-// var resolved = _.resolver.resolve( src, 'dir/val1' )
-// console.log( resolved );
-
-// /*
-// log : `Hello`
-// */
+require( 'wdocgenerator' );
 
 console.log( 'not implemented' );
